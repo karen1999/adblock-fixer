31748b0f8d5e34bbf3385dd4066ac9f9186516df
Karen <karen.trainingaitc@gmail.com>
2018-11-12 15:04:03 +0400

    first

--- adblock_fixer.js
+++
@@ -1,7 +0,0 @@
-$('body').append('<div id="content_adv">Adblock выключен!</div>');
-$('head').append('<link rel="stylesheet" type="text/css" href="style.css">');
-
-if (  $("#content_adv").css("display") == "none"){
-	$('body').empty();
-	$('body').append('<div class="adblock_info"><p>Для получения контента отключите Adblock.</p></div>')	
-}
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Adblock fixer demo example</title>
+	<link rel="stylesheet" type="text/css" href="adblock-fixer/adblock-fixer.css">
+	<script src="adblock-fixer/adblock-fixer.js"></script>
+</head>
+<body>
+	<header>
+		<h1>This is header content</h1>
+	</header>
+	<main>
+		<h2>This is main content</h2>
+	</main>
+	<footer>
+		<h3>This is footer content</h3>
+	</footer>
+	<style type="text/css">
+		header, main, footer {
+			background: #afbfd1;
+			padding: 15px;
+			border: 1px solid white;
+		}
+
+		h1, h2, h3 {
+			margin: 0;
+		}
+	</style>
+</body>
+</html>
